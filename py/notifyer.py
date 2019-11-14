42e3c9914c25cb0ae378eb2dc80314a7ec074aac
tishikawa <takayuki.ishikawa@gunosy.com>
2019-11-14 13:00:23 +0900

    fix formatting

--- py/notifyer.py
+++ py/notifyer.py
@@ -1,12 +1,13 @@
 """
 scraper.py の結果を Slack に通知する君
 """
-import requests, json
+import json
+import requests
 
 from config import NOTIFY_CHANNEL, WEBHOOK_URL
 
+
 def notify(message):
-  requests.post(WEBHOOK_URL, data = json.dumps({
-    'channel': NOTIFY_CHANNEL,
-    'text': message,
-  }))
+    requests.post(
+        WEBHOOK_URL, data=json.dumps({"channel": NOTIFY_CHANNEL, "text": message})
+    )
