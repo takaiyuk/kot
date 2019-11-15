d5409e829c6b390e125cafc979900e0a528cd1d6
tetsuya.motegi <tetsuya.motegi@ip-10-3-10-107.ap-northeast-1.compute.internal>
2019-11-15 14:26:17 +0900

    Change crawler

--- py/crawler.py
+++ py/crawler.py
@@ -67,7 +67,18 @@ class Crawler:
                 executable_path=AMAZONLINUX_DRIVER_PATH, options=options
             )
         else:
-            self.driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
+            options.add_argument("--disable-gpu")
+            options.add_argument("--window-size=1280x1696")
+            options.add_argument("--disable-application-cache")
+            options.add_argument("--disable-infobars")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--hide-scrollbars")
+            options.add_argument("--enable-logging")
+            options.add_argument("--log-level=0")
+            options.add_argument("--single-process")
+            options.add_argument("--ignore-certificate-errors")
+            options.add_argument("--homedir=/tmp")
+            self.driver = webdriver.Chrome(options=options)
         self.browser = Browser(self.driver)
 
     def get_source(self):
