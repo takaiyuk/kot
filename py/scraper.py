f7c1f1b129397d1dc690e5490b23896c7bfae73e
takaiyuk <takaishikawa1225@gmail.com>
2019-12-20 08:59:58 +0900

    Add Parser

--- py/scraper.py
+++ py/scraper.py
@@ -1,70 +1,13 @@
 # Import
 from bs4 import BeautifulSoup
-from collections import Counter
 
 from .const import WORK_HOUR
 from .crawler import Crawler
 
 
-class Scraper:
-    def __init__(self):
-        self.soup = None
-        self.holiday_count = 0  # TODO: 当月利用した有給日数を取得
-
-    def _clean_text(self, x):
-        x = x.replace("\n", "")
-        x = x.replace(" ", "")
-        x = x.strip()
-        return x
-
-    def _hour_to_minute(self, hours):
-        return round(hours // 1.0 * 60 + hours % 1.0 * 100)
-
-    def _minute_to_hour(self, minutes):
-        is_minus = False
-        if minutes < 0:
-            is_minus = True
-            minutes *= -1
-        hm = minutes // 60 + round(minutes % 60 / 100, 2)
-        if is_minus:
-            hm *= -1
-        return hm
-
-    def _str_to_int(self, string):
-        return int(float(string))
-
-    def _change_notation(self, str_time):
-        """
-        2.31 -> 2時間31分
-        """
-        str_time = str(str_time)
-        return f'{str_time.split(".")[0]}時間{str_time.split(".")[1]}分'
-
-    def calc_monthly_work_count(self, monthly_work_hour):
-        monthly_work_count = monthly_work_hour / WORK_HOUR
-        return round(monthly_work_count, 2)
-
-    def calc_count_remain(self, monthly_work_count, work_count):
-        return round(monthly_work_count - work_count, 2)
-
-    def calc_hour_remain(self, total_hours, finished_hours):
-        total_minutes = self._hour_to_minute(total_hours)
-        finished_minites = self._hour_to_minute(finished_hours)
-        remain_minutes = total_minutes - finished_minites
-        remain_hour = self._minute_to_hour(remain_minutes)
-        return round(remain_hour, 2)
-
-    def calc_hour_remain_by_day(self, remain_hours, remain_count):
-        remain_minutes = self._hour_to_minute(remain_hours)
-        remain_minutes_by_day = remain_minutes / remain_count
-        remain_hours_by_day = self._minute_to_hour(remain_minutes_by_day)
-        return round(remain_hours_by_day, 2)
-
-    def calc_saving_time(self, work_hour, work_count):
-        saving_time = self._hour_to_minute(work_hour) - self._hour_to_minute(
-            WORK_HOUR * work_count
-        )
-        return round(self._minute_to_hour(saving_time), 2)
+class Parser:
+    def __init__(self, html: str):
+        self.soup = BeautifulSoup(html, "html.parser")
 
     def get_holiday_count(self):
         holiday_counts = self.soup.find_all("div", class_="holiday_count")
@@ -144,25 +87,98 @@ class Scraper:
         )
         return start_time_string, teiji_time_string
 
-    def raw_data(self, html=None):
-        if html is None:
-            html = Crawler().get_source()
-        self.soup = BeautifulSoup(html, "html.parser")
-
+    def parse(self):
         # 有給等の取得日数を取得
         self.get_holiday_count()
 
         # 今月の必要勤務時間を取得
         monthly_work_hours = self.get_monthly_work_hour()
 
-        # 今月の必要勤務日を計算
-        monthly_work_count = self.calc_monthly_work_count(monthly_work_hours)
-
         # 前日までの勤務日数を取得
         work_count = self.get_work_count()
 
         # 前日までの勤務時間を取得
         work_hours = self.get_work_hour()
+
+        # 当日の出勤打刻時間
+        try:
+            start_time, teiji_time = self.get_today_work_start()
+        except Exception:
+            print("打刻しましたか？退勤後なら問題ないですが")
+            start_time, teiji_time = None, None
+
+
+class Aggregator:
+    def __init__(self):
+        pass
+
+
+class Scraper:
+    def __init__(self):
+        self.soup = None
+        self.holiday_count = 0
+
+    def _clean_text(self, x):
+        x = x.replace("\n", "")
+        x = x.replace(" ", "")
+        x = x.strip()
+        return x
+
+    def _hour_to_minute(self, hours):
+        return round(hours // 1.0 * 60 + hours % 1.0 * 100)
+
+    def _minute_to_hour(self, minutes):
+        is_minus = False
+        if minutes < 0:
+            is_minus = True
+            minutes *= -1
+        hm = minutes // 60 + round(minutes % 60 / 100, 2)
+        if is_minus:
+            hm *= -1
+        return hm
+
+    def _str_to_int(self, string):
+        return int(float(string))
+
+    def _change_notation(self, str_time):
+        """
+        2.31 -> 2時間31分
+        """
+        str_time = str(str_time)
+        return f'{str_time.split(".")[0]}時間{str_time.split(".")[1]}分'
+
+    def calc_monthly_work_count(self, monthly_work_hour):
+        monthly_work_count = monthly_work_hour / WORK_HOUR
+        return round(monthly_work_count, 2)
+
+    def calc_count_remain(self, monthly_work_count, work_count):
+        return round(monthly_work_count - work_count, 2)
+
+    def calc_hour_remain(self, total_hours, finished_hours):
+        total_minutes = self._hour_to_minute(total_hours)
+        finished_minites = self._hour_to_minute(finished_hours)
+        remain_minutes = total_minutes - finished_minites
+        remain_hour = self._minute_to_hour(remain_minutes)
+        return round(remain_hour, 2)
+
+    def calc_hour_remain_by_day(self, remain_hours, remain_count):
+        remain_minutes = self._hour_to_minute(remain_hours)
+        remain_minutes_by_day = remain_minutes / remain_count
+        remain_hours_by_day = self._minute_to_hour(remain_minutes_by_day)
+        return round(remain_hours_by_day, 2)
+
+    def calc_saving_time(self, work_hour, work_count):
+        saving_time = self._hour_to_minute(work_hour) - self._hour_to_minute(
+            WORK_HOUR * work_count
+        )
+        return round(self._minute_to_hour(saving_time), 2)
+
+    def raw_data(self, html=None):
+        if html is None:
+            html = Crawler().get_source()
+
+        # 今月の必要勤務日を計算
+        monthly_work_count = self.calc_monthly_work_count(monthly_work_hours)
 
         # 残り日数と残り必要時間、1日あたりの必要時間を計算
         work_count_remain = self.calc_count_remain(monthly_work_count, work_count)
@@ -173,13 +189,6 @@ class Scraper:
 
         # 暫定残業時間を計算
         saving_time = self.calc_saving_time(work_hours, work_count)
-
-        # 当日の出勤打刻時間
-        try:
-            start_time, teiji_time = self.get_today_work_start()
-        except Exception:
-            print("打刻しましたか？退勤後なら問題ないですが")
-            start_time, teiji_time = None, None
 
         results = {
             "work_count_remain": work_count_remain,
