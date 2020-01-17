db2c546036921a1eedd6ac8bf699181e6b63ecad
Yuto Suzuki <yuutoo.advance@gmail.com>
2020-01-17 18:55:38 +0900

    check if exist

--- console.sh
+++ console.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-
-docker run -v "${HOME}/.scrape_kot/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot run.py console
+DIR=${HOME}/.scrape_kot
+if [ ! -f "$DIR/config.py" ]; then
+  DIR=${PWD}
+fi
+echo $DIR
+docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot run.py console
 
--- notify.sh
+++ notify.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+DIR=${HOME}/.scrape_kot
+if [ ! -f "$DIR/config.py" ]; then
+  DIR=${PWD}
+fi
 
-docker run -v "${HOME}/.scrape_kot/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot run.py
+docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot run.py
 
