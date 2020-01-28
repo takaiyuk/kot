243dcb16ffc6afc04c209a91da81e1dca08f0486
tishikawa <takayuki.ishikawa@gunosy.com>
2020-01-28 13:09:19 +0900

    Change default notify

--- scrapekot.sh
+++ scrapekot.sh
@@ -4,12 +4,16 @@ if [ ! -f "$DIR/config.py" ]; then
   DIR=${PWD}
 fi
 
-if [ "$1" == "notify" ] || [ "$1" == "console" ]; then
-  echo $1
+SUBCMD="notify"
+if [ "$1" == "" ] || [ "$1" == "notify" ]; then
+  echo ${SUBCMD}
+elif [ "$1" == "console" ]; then
+  SUBCMD="console"
+  echo ${SUBCMD}
 else
-  echo "Error: you need one of the following arguments: 'notify' or 'console'"
+  echo "Error: you need one of the following arguments: '', 'notify' or 'console'"
   exit 1
 fi
 
-docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot run.py $1
+docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot run.py ${SUBCMD}
 
