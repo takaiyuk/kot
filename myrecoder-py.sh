fc5eae7904f748d963a73f349645fba103818d96
tishikawa <takayuki.ishikawa@gunosy.com>
2020-02-19 18:31:52 +0900

    Change shell scripts

--- myrecoder-py.sh
+++ myrecoder-py.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-python -m my_recorder.run -c "$1"
+if [ "$2" == "-y" ]; then
+  python -m my_recorder.run -c "$1" "$2"
+else
+  python -m my_recorder.run -c "$1"
+fi
--- myrecoder.sh
+++ myrecoder.sh
@@ -5,4 +5,9 @@ if [ ! -f "$DIR/config.py" ]; then
   DIR=${PWD}
 fi
 
-docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot -m my_recorder.run -c "$1"
+if [ "$2" == "-y" ]; then
+  docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot -m my_recorder.run -c "$1" "$2"
+else
+  docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot -m my_recorder.run -c "$1"
+fi
+
