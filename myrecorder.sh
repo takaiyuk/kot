ac0e3f4023c8ccc4a8a6f482b5e86335f8b02bf3
tishikawa <takayuki.ishikawa@gunosy.com>
2020-05-02 17:44:53 +0900

    Add option for message

--- myrecorder.sh
+++ myrecorder.sh
@@ -5,9 +5,12 @@ if [ ! -f "$DIR/config.py" ]; then
   DIR=${PWD}
 fi
 
-if [ "$2" == "-y" ]; then
+if [ "$2" == "-y" ] && [ "$3" != "" ]; then
+  docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot -m my_recorder.run --cmd "$1" --yes --message "$3"
+elif [ "$2" == "-y" ]; then
   docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot -m my_recorder.run --cmd "$1" --yes
+elif [ "$2" != "" ]; then
+  docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot -m my_recorder.run --cmd "$1" --message "$2"
 else
   docker run -v "${DIR}/config.py":/scrape_kot/config.py -it --rm takaiyuk/scrape-kot -m my_recorder.run --cmd "$1"
 fi
-
