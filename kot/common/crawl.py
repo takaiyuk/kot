4f3830f48e0223ac0b33fe2d5b8502279eaf3ba5
takaiyuk <takaishikawa1225@gmail.com>
2022-08-19 00:17:36 +0900

    Use s3 as cache saving repository for driver

--- kot/common/crawl.py
+++ kot/common/crawl.py
@@ -1,9 +1,11 @@
+import os
 import random
 import time
 from dataclasses import dataclass
 from enum import Enum
 from typing import Any, Type, TypeVar, Union
 
+import boto3
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -14,6 +16,9 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 TOP_URL = "https://s3.kingtime.jp/admin"
 DRIVER_PATH = "/tmp"
+_S3_CACHE_DIR = os.path.join(os.getenv("S3_CACHE_BUCKET", ""), "wdm/drivers/chromedriver/linux64")
+DRIVER_VERSION = os.getenv("DRIVER_VERSION", "")
+S3_CACHE_PATH = os.path.join(_S3_CACHE_DIR, DRIVER_VERSION, "chromedriver")
 B = TypeVar("B", bound="Browser")
 
 
@@ -34,7 +39,33 @@ class Driver:
     @classmethod
     def build(cls, driver_options: DriverOptions) -> Union[webdriver.Chrome, webdriver.Firefox]:
         browser_options = cls._get_browser_options(driver_options)
+        """
+        NOTE:
+            * cache の確認
+                * S3 に cache があればそれを DRIVER_PATH にダウンロードする
+                    * set_cache_flag を下げる
+                * cache がなければ何もしない
+                    * set_cache_flag を立てる
+            * cls._get_driver を実行
+            * cache を S3 に保存する
+                * set_cache_flag が立っていれば S3 に保存する
+                * set_cache_flag が立っていなければ何もしない
+
+        s3: $S3_CACHE_PATH/chromedriver
+        local: $DRIVER_PATH/.wdm/drivers/chromedriver/linux64/$DRIVER_VERSION/chromedriver
+        """
+        driver_save_path = os.path.join(
+            DRIVER_PATH, ".wdm/drivers/chromedriver/linux64", DRIVER_VERSION, "chromedriver"
+        )
+        set_cache_flag = True
+        # cache が存在しているかつ、Lambda で実行時に cache を利用する。set_cache_flag は False にする。
+        if check_s3_path_exists(S3_CACHE_PATH) and os.getenv("LAMBDA_TASK_ROOT"):
+            cls.download_driver(S3_CACHE_PATH, driver_save_path)
+            set_cache_flag = False
         driver = cls._get_driver(driver_options, browser_options)
+        # set_cache_flag が True かつ、Lambda で実行時に cache を保存する。
+        if set_cache_flag and os.getenv("LAMBDA_TASK_ROOT"):
+            cls.upload_driver(driver_save_path, S3_CACHE_PATH)
         return driver
 
     @classmethod
@@ -99,6 +130,20 @@ class Driver:
             )
         return driver
 
+    @classmethod
+    def download_driver(cls, s3_path: str, local_path: str) -> None:
+        bucket = s3_path.split("/")[0]
+        key = s3_path.split("/")[1:]
+        s3 = boto3.client("s3")
+        s3.download_file(bucket, key, local_path)
+
+    @classmethod
+    def upload_driver(cls, local_path: str, s3_path: str) -> None:
+        bucket = s3_path.split("/")[0]
+        key = s3_path.split("/")[1:]
+        s3_client = boto3.client("s3")
+        s3_client.upload_file(local_path, bucket, key)
+
 
 class Browser:
     def __init__(self, driver: Union[webdriver.Chrome, webdriver.Firefox]) -> None:
@@ -157,3 +202,15 @@ class BaseCrawler:
 
     def run(self, params: Any) -> Any:
         raise NotImplementedError
+
+
+def check_s3_path_exists(path: str) -> bool:
+    bucket = path.split("/")[0]
+    key = path.split("/")[1:]
+
+    client = boto3.client("s3")
+    result = client.list_objects(Bucket=bucket, Prefix=key)
+    if "Contents" in result:
+        return True
+    else:
+        return False
