d4c97cb7f2fcb6201f37a10293312c240f220938
takaiyuk <takaishikawa1225@gmail.com>
2023-07-05 09:40:08 +0900

    Implement retry for crawler

--- kot/myrecorder/crawl.py
+++ kot/myrecorder/crawl.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from tenacity import retry, stop_after_attempt, wait_fixed
+
 from kot.common.crawl import BaseCrawler
 from kot.common.logger import logger
 from kot.myrecorder import MyRecorderOptions
@@ -19,43 +21,44 @@ class CrawlerParams:
 
 class Crawler(BaseCrawler):
     def run(self, params: CrawlerParams) -> bool:
-        is_punched = self._punch(params)
+        try:
+            is_punched = self._punch(params)
+        finally:
+            # ウェブドライバーのプロセスを消す
+            self.browser.quit()
         return is_punched
 
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(3))
     def _punch(self, params: CrawlerParams) -> bool:
-        try:
-            # トップページ
-            self.browser.get(TOP_URL)
-            # ID/PASS 入力
-            self.browser.send('//*[@id="id"]', params.account_id)
-            self.browser.send('//*[@id="password"]', params.account_password)
-            # ログイン
-            self.browser.click('//*[@id="modal_window"]/div/div/div[3]/div/div')
-            """
-            # TODO: ログイン成功したか確認
-            # 成功時もURLの遷移なし・失敗時もモーダルが出るだけなのでseleniumでのログインの成否判断が難しい
-            """
-            myrecoder_option = getattr(MyRecorderOptions, params.command)
-            # 確認する
-            if params.yes:
-                val = "y"
-                self.browser.sleep()
+        # トップページ
+        self.browser.get(TOP_URL)
+        # ID/PASS 入力
+        self.browser.send('//*[@id="id"]', params.account_id)
+        self.browser.send('//*[@id="password"]', params.account_password)
+        # ログイン
+        self.browser.click('//*[@id="modal_window"]/div/div/div[3]/div/div')
+        """
+        # TODO: ログイン成功したか確認
+        # 成功時もURLの遷移なし・失敗時もモーダルが出るだけなのでseleniumでのログインの成否判断が難しい
+        """
+        myrecoder_option = getattr(MyRecorderOptions, params.command)
+        # 確認する
+        if params.yes:
+            val = "y"
+            self.browser.sleep()
+        else:
+            val = input(f"{myrecoder_option.name}ボタンを押していいですか？[y/n]: ")
+        # 実行する
+        if val != "y":
+            logger.info(f"{myrecoder_option.name}ボタンはスキップしました")
+            return False
+        else:
+            assert params.command in MyRecorderOptions.__annotations__.keys()
+            xpath = myrecoder_option.xpath
+            if not params.is_debug:
+                self.browser.click(xpath)
+                logger.info(f"{myrecoder_option.name}ボタンが押されました（多分）")
+                return True
             else:
-                val = input(f"{myrecoder_option.name}ボタンを押していいですか？[y/n]: ")
-            # 実行する
-            if val != "y":
-                logger.info(f"{myrecoder_option.name}ボタンはスキップしました")
+                logger.info(f"{myrecoder_option.name}ボタンは押されない")
                 return False
-            else:
-                assert params.command in MyRecorderOptions.__annotations__.keys()
-                xpath = myrecoder_option.xpath
-                if not params.is_debug:
-                    self.browser.click(xpath)
-                    logger.info(f"{myrecoder_option.name}ボタンが押されました（多分）")
-                    return True
-                else:
-                    logger.info(f"{myrecoder_option.name}ボタンは押されない")
-                    return False
-        finally:
-            # プロセス消す
-            self.browser.quit()
