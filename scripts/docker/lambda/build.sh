0f32a04d548a91726d3c89b8687f30372d91d41b
takaiyuk <takaishikawa1225@gmail.com>
2023-10-28 23:48:26 +0900

    Fix lambda dockerfile

--- scripts/docker/lambda/build.sh
+++ scripts/docker/lambda/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 source ./scripts/docker/lambda/.env
 
-docker build -f ./docker/lambda/Dockerfile -t $IMAGE:$TAG .
+# NOTE: --platform linux/x86_64 is required for chromium to work
+docker build -f ./docker/lambda/Dockerfile -t $IMAGE:$TAG --platform linux/x86_64 .
